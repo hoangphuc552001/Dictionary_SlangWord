a19f7a8a03e4b98f72fd2278e76218667528d888
hoangphuc552001@gmail.com <hoangphuc552001@gmail.com>
2021-12-15 16:40:51 +0700

    ok

--- src/RandomBySW.java
+++ src/RandomBySW.java
@@ -19,6 +19,7 @@ public class RandomBySW extends JFrame implements ActionListener {
     SlangWord slangWord = SlangWord.getInstance();
     int check = 0;
     String[] quizChoice;
+    String answer;
 
     /**
      * default constructor
@@ -69,7 +70,7 @@ public class RandomBySW extends JFrame implements ActionListener {
         back.setBackground(new Color(232, 55, 35));
         back.setForeground(Color.white);
         back.setUI(new stylebutton());
-        back.setPreferredSize(new Dimension(70, 30));
+        back.setPreferredSize(new Dimension(90, 50));
         //
         next = new JButton("Next");
         next.setAlignmentX(CENTER_ALIGNMENT);
@@ -78,7 +79,7 @@ public class RandomBySW extends JFrame implements ActionListener {
         next.setBackground(new Color(0, 255, 119, 157));
         next.setForeground(Color.white);
         next.setUI(new stylebutton());
-        next.setPreferredSize(new Dimension(70, 30));
+        next.setPreferredSize(new Dimension(90, 50));
         //
         answerA = new JButton();
         answerA.setAlignmentX(CENTER_ALIGNMENT);
@@ -153,6 +154,19 @@ public class RandomBySW extends JFrame implements ActionListener {
         answerC.setText(quizChoice[3]);
         answerD.setText(quizChoice[4]);
         titSW.setText(quizChoice[0]);
+        answer = quizChoice[5];
+        if (answerA.getText().equals(answer)){
+            check = 1;
+        }
+        if (answerB.getText().equals(answer)) {
+            check = 2;
+        }
+        if (answerC.getText().equals(answer)) {
+            check = 3;
+        }
+        if (answerD.getText().equals(answer)) {
+            check = 4;
+        }
         answerA.setFont(new Font("Monaco", Font.BOLD, 30));
         answerB.setFont(new Font("Monaco", Font.BOLD, 30));
         answerC.setFont(new Font("Monaco", Font.BOLD, 30));
@@ -161,7 +175,7 @@ public class RandomBySW extends JFrame implements ActionListener {
         this.setTitle("Random Slang Word");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setVisible(true);
-        this.setSize(560, 400);
+        this.setSize(860, 550);
         Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
         this.setLocation(dim.width / 2 - this.getSize().width / 2, dim.height / 2 - this.getSize().height / 2);
     }
@@ -185,22 +199,14 @@ public class RandomBySW extends JFrame implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (answerA.getText().equals(quizChoice[5])) {
-            check = 1;
-        }
-        if (answerB.getText().equals(quizChoice[5])) {
-            check = 2;
-        }
-        if (answerC.getText().equals(quizChoice[5])) {
-            check = 3;
-        }
-        if (answerD.getText().equals(quizChoice[5])) {
-            check = 4;
-        }
         if (e.getSource().equals(back)) {
             this.dispose();
             MultipleChoice.GUI();
         } else if (e.getSource().equals(next)) {
+            answerA.setEnabled(true);
+            answerB.setEnabled(true);
+            answerC.setEnabled(true);
+            answerD.setEnabled(true);
             titSW.setText(quizChoice[0]);
             answerA.setBackground(new Color(18, 0, 147));
             answerB.setBackground(new Color(18, 0, 147));
@@ -210,14 +216,25 @@ public class RandomBySW extends JFrame implements ActionListener {
             answerB.setFont(new Font("Monaco", Font.BOLD, 30));
             answerC.setFont(new Font("Monaco", Font.BOLD, 30));
             answerD.setFont(new Font("Monaco", Font.BOLD, 30));
-            answerA.setEnabled(true);
-            answerB.setEnabled(true);
-            answerC.setEnabled(true);
-            answerD.setEnabled(true);
             answerA.setText(quizChoice[1]);
             answerB.setText(quizChoice[2]);
             answerC.setText(quizChoice[3]);
             answerD.setText(quizChoice[4]);
+            quizChoice = slangWord.randomMultipleChoice(1);
+            String answer1 = quizChoice[5];
+            if (answerA.getText().equals(answer)){
+                check = 1;
+            }
+            if (answerB.getText().equals(answer)) {
+                check = 2;
+            }
+            if (answerC.getText().equals(answer)) {
+                check = 3;
+            }
+            if (answerD.getText().equals(answer)) {
+                check = 4;
+            }
+            answer=answer1;
         } else if (e.getSource().equals(answerA)) {
             if (check == 1) {
                 JOptionPane.showMessageDialog(null, "Correct Answer");
@@ -227,26 +244,18 @@ public class RandomBySW extends JFrame implements ActionListener {
                 answerA.setBackground(new Color(225, 10, 10));
                 if (check == 2) {
                     answerB.setBackground(new Color(0, 255, 102));
-                    answerA.setEnabled(false);
-                    answerB.setEnabled(false);
-                    answerC.setEnabled(false);
-                    answerD.setEnabled(false);
                 }
                 if (check == 3) {
                     answerC.setBackground(new Color(0, 255, 102));
-                    answerA.setEnabled(false);
-                    answerB.setEnabled(false);
-                    answerC.setEnabled(false);
-                    answerD.setEnabled(false);
                 }
                 if (check == 4) {
                     answerD.setBackground(new Color(0, 255, 102));
-                    answerA.setEnabled(false);
-                    answerB.setEnabled(false);
-                    answerC.setEnabled(false);
-                    answerD.setEnabled(false);
                 }
             }
+            answerA.setEnabled(false);
+            answerB.setEnabled(false);
+            answerC.setEnabled(false);
+            answerD.setEnabled(false);
         } else if (e.getSource().equals(answerB)) {
             if (check == 2) {
                 JOptionPane.showMessageDialog(null, "Correct Answer");
@@ -256,26 +265,18 @@ public class RandomBySW extends JFrame implements ActionListener {
                 answerB.setBackground(new Color(225, 10, 10));
                 if (check == 1) {
                     answerA.setBackground(new Color(0, 255, 102));
-                    answerA.setEnabled(false);
-                    answerB.setEnabled(false);
-                    answerC.setEnabled(false);
-                    answerD.setEnabled(false);
                 }
                 if (check == 3) {
                     answerC.setBackground(new Color(0, 255, 102));
-                    answerA.setEnabled(false);
-                    answerB.setEnabled(false);
-                    answerC.setEnabled(false);
-                    answerD.setEnabled(false);
                 }
                 if (check == 4) {
                     answerD.setBackground(new Color(0, 255, 102));
-                    answerA.setEnabled(false);
-                    answerB.setEnabled(false);
-                    answerC.setEnabled(false);
-                    answerD.setEnabled(false);
                 }
             }
+            answerA.setEnabled(false);
+            answerB.setEnabled(false);
+            answerC.setEnabled(false);
+            answerD.setEnabled(false);
         } else if (e.getSource().equals(answerC)) {
             if (check == 3) {
                 JOptionPane.showMessageDialog(null, "Correct Answer");
@@ -285,62 +286,48 @@ public class RandomBySW extends JFrame implements ActionListener {
                 answerC.setBackground(new Color(225, 10, 10));
                 if (check == 1) {
                     answerA.setBackground(new Color(0, 255, 102));
-                    answerA.setEnabled(false);
-                    answerB.setEnabled(false);
-                    answerC.setEnabled(false);
-                    answerD.setEnabled(false);
+
                 }
                 if (check == 2) {
                     answerB.setBackground(new Color(0, 255, 102));
-                    answerA.setEnabled(false);
-                    answerB.setEnabled(false);
-                    answerC.setEnabled(false);
-                    answerD.setEnabled(false);
+
                 }
                 if (check == 4) {
                     answerD.setBackground(new Color(0, 255, 102));
-                    answerA.setEnabled(false);
-                    answerB.setEnabled(false);
-                    answerC.setEnabled(false);
-                    answerD.setEnabled(false);
+
                 }
             }
-        }
+            answerA.setEnabled(false);
+            answerB.setEnabled(false);
+            answerC.setEnabled(false);
+            answerD.setEnabled(false);
+        } else if (e.getSource().equals(answerD)) {
+            if (check == 4) {
+                JOptionPane.showMessageDialog(null, "Correct Answer");
+                answerD.setBackground(new Color(0, 255, 102));
+            } else {
+                JOptionPane.showMessageDialog(null, "Incorred Answer", "Alert", JOptionPane.WARNING_MESSAGE);
+                answerD.setBackground(new Color(225, 10, 10));
+                if (check == 1) {
+                    answerA.setBackground(new Color(0, 255, 102));
 
-    else if(e.getSource().equals(answerD))
-        if (check == 4) {
-            JOptionPane.showMessageDialog(null, "Correct Answer");
-            answerD.setBackground(new Color(0, 255, 102));
-        } else {
-            JOptionPane.showMessageDialog(null, "Incorred Answer", "Alert", JOptionPane.WARNING_MESSAGE);
-            answerD.setBackground(new Color(225, 10, 10));
-            if (check == 1) {
-                answerA.setBackground(new Color(0, 255, 102));
-                answerA.setEnabled(false);
-                answerB.setEnabled(false);
-                answerC.setEnabled(false);
-                answerD.setEnabled(false);
-            }
-            if (check == 2) {
-                answerB.setBackground(new Color(0, 255, 102));
-                answerA.setEnabled(false);
-                answerB.setEnabled(false);
-                answerC.setEnabled(false);
-                answerD.setEnabled(false);
-            }
-            if (check == 3) {
-                answerC.setBackground(new Color(0, 255, 102));
-                answerA.setEnabled(false);
-                answerB.setEnabled(false);
-                answerC.setEnabled(false);
-                answerD.setEnabled(false);
-            }
-        }
+                }
+                if (check == 2) {
+                    answerB.setBackground(new Color(0, 255, 102));
+                }
+                if (check == 3) {
+                    answerC.setBackground(new Color(0, 255, 102));
 
-        quizChoice = slangWord.randomMultipleChoice(1);
-        String answer = quizChoice[5];
+                }
+
+            }
+
+            answerA.setEnabled(false);
+            answerB.setEnabled(false);
+            answerC.setEnabled(false);
+            answerD.setEnabled(false);
+        }
     }
-
     public static void main(String[] args) {
         RandomBySW.GUI();
     }
